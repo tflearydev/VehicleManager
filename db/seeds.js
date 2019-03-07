bcccb88ba6c6b36b3194c9fcfe985b559e4e6241
TFlearyDev <tflearydev@gmail.com>
2019-03-07 12:03:53 -0500

    preparing to add vehicles and vehicle model

--- db/seeds.js
+++ db/seeds.js
@@ -4,6 +4,26 @@ const Customer = require("../models/Customer.js");
 
 // const lambo = new Vehicle({
 //   name: "2019 Lamborghini Urus",
+//   color: "Forest Green"
+// });
+
+// const mclaren = new Vehicle({
+//   name: "2016 Mclaren 570s",
+//   color: "Candy Apple Red"
+// });
+
+// const aston = new Vehicle({
+//   name: "2016 Aston Martin DB9",
+//   color: "Gun Metal Gray"
+// });
+
+// const audi = new Vehicle({
+//   name: "2014 Audi A5 Coupe",
+//   color: "Pearl White"
+// });
+///////
+// const lambo = new Vehicle({
+//   name: "2019 Lamborghini Urus",
 //   year: "5.99"
 // });
 
@@ -27,7 +47,8 @@ const michael = new Customer({
   email: "michaelsmith@gmail.com",
   phone: 6787578888,
   imgLink: "https://www.4wheelsnews.com/images/photo_galleries/large/28230/topcar-lamborghini-urus-0.jpg",
-  // vehicleList: [toyota]
+  // vehicleList: [lambo]
+  // serviceList: [oil]
 });
 
 const alexander = new Customer({
@@ -35,7 +56,8 @@ const alexander = new Customer({
   email: "alexander@gmail.com",
   phone: 4043333333,
   imgLink: "http://www.autotribute.com/wp-content/uploads/2015/04/McLaren-540C-supercar-coupe.jpg",
-  // vehicleList: [padThai]
+  // vehicleList: [mclaren]
+  // serviceList: [brakes]
 });
 
 const james = new Customer({
@@ -43,23 +65,29 @@ const james = new Customer({
   email: "jamesoconner@gmail.com",
   phone: 4045555555,
   imgLink: "http://www.allwhitebackground.com/images/1/Aston-Martin-5.png",
-  // vehicleList: [fishTaco]
+  // vehicleList: [aston]
+  // serviceList: [wash]
 });
 
 const chris = new Customer({
   name: "Chris Henrick",
   email: "chenrick@gmail.com",
   phone: 6780008889,
-  imgLink: "https://images.hgmsites.net/lrg/2015-audi-a7-4-door-hb-quattro-3-0-prestige-angular-front-exterior-view_100475579_l.jpg",
-  // foodItems: [injera]
+  imgLink: "https://purepng.com/public/uploads/large/purepng.com-white-audiaudicarluxurious-carprofessional-carwhite-audi-1701527409201rfscd.png",
+  // vehicleList: [audi]
+  // serviceList: [alignment]
 });
 
+// foodItems: [injera]
+
 Customer.remove({})
-//   .then(() => Food.remove({}))
+//   .then(() => Vehicle.remove({}))
   .then(() => Customer.create([michael, alexander, james, chris]))
-//   .then(() => Food.insertMany([pizza, injera, padThai, fishTaco]))
+//   .then(() => Vehicle.insertMany([lambo, mclaren, aston, audi]))
   .then(() => {
     console.log("seeded successfully");
     mongoose.connection.close();
   })
   .catch(err => console.log(err, "error!"));
+
+  
