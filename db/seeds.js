b7f78ff08e252b11e3a153a739e714c210f0dd7a
TFlearyDev <tflearydev@gmail.com>
2019-03-12 00:43:01 -0400

    heroku

--- db/seeds.js
+++ db/seeds.js
@@ -1,7 +1,7 @@
 const mongoose = require("./connection.js");
 const Shop = require("../models/shop.js")
-const Customer = require("../models/Customer.js");
-const Vehicle = require("../models/Vehicle.js");
+const Customer = require("../models/customer.js");
+const Vehicle = require("../models/vehicle.js");
 
 
 const mclaren = new Vehicle({
