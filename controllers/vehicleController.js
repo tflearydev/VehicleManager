28adf41d36bc179fa9acb23b5eb50f530e50b759
TFlearyDev <tflearydev@gmail.com>
2019-03-30 21:47:35 -0400

    preparing to add an edit and update uption to each of the businesses' vehicles in their sales lot, as they can already edit and update each customer that they service

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -22,16 +22,16 @@ const vehicleController = {
         })
     },
     // edit and update currently disabled, will add with updates
-    // edit: (req, res) => {
-    //     Vehicle.findById(req.params.vehicleId).then((vehicle) => {
-    //         res.render('vehicles/edit', {vehicle})
-    //     })
-    // },
-    // update: (req, res) => {
-    //     Vehicle.findByIdAndUpdate(req.params.vehicleId, req.body, {new: true}).then((updatedVehicle) => {
-    //         res.redirect(`/${req.params.vehicleId}`)
-    //     })
-    // },
+    edit: (req, res) => {
+        Vehicle.findById(req.params.vehicleId).then((vehicle) => {
+            res.render('vehicles/edit', {vehicle})
+        })
+    },
+    update: (req, res) => {
+        Vehicle.findByIdAndUpdate(req.params.vehicleId, req.body, {new: true}).then((updatedVehicle) => {
+            res.redirect(`/${req.params.vehicleId}`)
+        })
+    },
     delete: (req, res) => {
         Vehicle.findByIdAndDelete(req.params.vehicleId).then(() => {
             console.log(`deleted vehicle with id of ${req.params.vehicleId}`)
